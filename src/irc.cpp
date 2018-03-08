54b2f0a050a0987c37a8e64606292536f9d99ce8
Jacob Myeonghwan Lee <reisaiah@gmail.com>
2018-03-08 11:02:54 +0900

    설정 값 변경.

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #ZonberCoinTEST3\r");
-            Send(hSocket, "WHO #ZonberCoinTEST3\r");
+            Send(hSocket, "JOIN #SponsorCoinTEST3\r");
+            Send(hSocket, "WHO #SponsorCoinTEST3\r");
         } else {
-            // randomly join #ZonberCoin00-#ZonberCoin99
+            // randomly join #SponsorCoin00-#SponsorCoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #ZonberCoin%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #ZonberCoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #SponsorCoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #SponsorCoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
